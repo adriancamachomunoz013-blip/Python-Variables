fa5677af6c4eb68a43930222e1caffcf27c41c98
adriancamachomunoz013-blip <adriancamachomunoz013@gmail.com>
2026-07-13 22:09:04 +0200

    Add files via upload

---
+++ Variables-2.py
@@ -0,0 +1,7 @@
+nombre = 'Mario' #Vale int,float,bool,string
+bienvenida = f"Hola {nombre} ¿Cómo estás?"
+del nombre #borra variables
+print(bienvenida)
+print("hola" not in bienvenida) #True or False
+print("hola" in bienvenida) #True or False
+#operadores de pertenencia in or not in
