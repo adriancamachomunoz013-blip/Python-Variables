aa254ae61844dd530f45a4f8ab6c76d177532d22
adriancamachomunoz013-blip <adriancamachomunoz013@gmail.com>
2026-07-13 22:31:58 +0200

    Introduce variables and user input in Variables-2.py
    Added a new program that introduces variables, concatenates strings, and gathers user input.

--- Variables-2.py
+++ Variables-2.py
@@ -1,7 +1,14 @@
-nombre = 'Mario' #Vale int,float,bool,string
-bienvenida = f"Hola {nombre} ¿Cómo estás?"
-del nombre #borra variables
-print(bienvenida)
-print("hola" not in bienvenida) #True or False
-print("hola" in bienvenida) #True or False
-#operadores de pertenencia in or not in
+# Hello, this is my second program in python. Today, in this file I'm going to use strings.
+first_name = "Greg"
+second_name = "House"
+full_name = first_name + " " + second_name
+age = 47
+person_info = full_name + " is " + age + " years old"
+job_name = doctor
+job_experience = "22 years of experience"
+job_info = "Greg is a " + job_name + " with " + job_experience
+
+know = input("What do you want to know?: ") == str(first_name, second_name, full_name, age, person_info, job_name, job_experience, job_info)
+print(know)
+
+
