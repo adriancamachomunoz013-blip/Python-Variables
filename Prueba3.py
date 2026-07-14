2f5dc1306676ebcc02a494331d374ef802ec34d9
adriancamachomunoz013-blip <adriancamachomunoz013@gmail.com>
2026-07-14 10:40:43 +0200

    Add files via upload

---
+++ Prueba3.py
@@ -0,0 +1,10 @@
+Diccionario1 = {
+    "Nickname": "Donald_gamer123",
+    "Password": "Gamerpresident123",
+    "First name": "Donald",
+    "Last name": "Duck"
+}
+if input("Tell me your nickname:") == Diccionario1["Nickname"]:
+    if input("Tell me your password:") == Diccionario1["Password"]:
+        print("Welcome")
+        
